408e0f09562311d84d6ae4408d3f7b2090a54da6
Joseph Wain <jpwain@gmail.com>
2026-05-22 16:49:41 -0700

    chore(release): update homebrew formula for v1.2.0

--- Formula/bucket.rb
+++ Formula/bucket.rb
@@ -5,15 +5,15 @@
 class Bucket < Formula
   desc "Terminal tool for reviewing two text files side by side"
   homepage "https://github.com/jpwain/bucket"
-  version "1.1.0"
+  version "1.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/jpwain/bucket/releases/download/v#{version}/bucket_#{version}_darwin_arm64.tar.gz"
-      sha256 "89af4d96ace8efa384034aaf4fe072a67f3b5ac568aa9baefd34d608326e9c86"
+      sha256 "52450d3b3af19b69b0ce97aca65f761ec59b12a97b8270e86d4a7512a1ac4a51"
     else
       url "https://github.com/jpwain/bucket/releases/download/v#{version}/bucket_#{version}_darwin_amd64.tar.gz"
-      sha256 "e8ae44ff3ca073aeee060bfb19cb6717d1cadb91ad9f85d518e35af390d3de48"
+      sha256 "b7df067d2e97e28ab4158a389534b167919cbc08249c6621670a960eda129850"
     end
   end
 
